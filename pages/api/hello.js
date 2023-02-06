3feb1f3ac2376ec8b0da653e51247b4ed856f1e3
PabloMesquida <pablomesquida67@gmail.com>
2023-02-06 12:34:07 +0100

    mysql-connection-test00

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,6 +4,5 @@ export default function handler(req, res) {
   const result = pool.query("SELECT NOW()");
 
   console.log(result);
-  res.status(200).json({ name: "John Doe" });
-  return <div>{result}</div>;
+  return <div>TEST: {result}</div>;
 }
