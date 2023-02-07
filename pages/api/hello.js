b838e418cd5058f17947474b88a4bd6f6270bc70
PabloMesquida <pablomesquida67@gmail.com>
2023-02-07 11:01:09 +0100

    variables de entorno

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,11 +3,11 @@ import mysql from "mysql2/promise";
 
 export default async function handler(req, res) {
   const connection = await mysql.createConnection({
-    host: "160.153.72.136",
-    user: "stckctrl_test",
-    password: "g1*fgYpq+2v$",
-    port: "3306",
-    database: "test_db",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
   });
 
   try {
