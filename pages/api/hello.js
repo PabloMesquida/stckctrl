88d4f2967e092c94cec42846f8d4a05a6c215b00
PabloMesquida <pablomesquida67@gmail.com>
2023-02-07 11:11:53 +0100

    test-variables

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,11 +3,17 @@ import mysql from "mysql2/promise";
 
 export default async function handler(req, res) {
   const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // port: process.env.DB_PORT,
+    // database: process.env.DB_NAME,
+
+    host: "160.153.72.136",
+    user: "stckctrl_test",
+    password: "6tfVBjtQ6mX",
+    port: 3306,
+    database: "test_db",
   });
 
   try {
