1042a373292dc6702621f2ece5abec1c759ce6b9
Pablo Mesquida <pablomesquida67@gmail.com>
2023-03-18 13:26:37 +0100

    styles-list-itmes

--- components/stock/ItemProducts.js
+++ components/stock/ItemProducts.js
@@ -1,38 +1,53 @@
 import stylesGeneral from "@/styles/General.module.css";
-import { MdImage } from "react-icons/md";
+import {
+  MdImage,
+  MdAppRegistration,
+  MdModeEdit,
+  MdDeleteForever,
+  MdOutlineFindInPage,
+} from "react-icons/md";
 
 const ItemProducts = ({ product }) => {
-  console.log(product);
   return (
     <div className={stylesGeneral.item_container}>
-      <div className="bg-th-background rounded-md mr-2">
-        <MdImage size={64} />
-      </div>
-      <div className="grow">
-        <div className="flex flex-row w-full">
-          <div>
-            <span>{product.codigo}</span>
-          </div>
-          <div>
-            <span>{product.categoria}</span>
-          </div>
-          <div>
-            <span>{product.nombre}</span>
+      <div className="flex grow">
+        <div>
+          <div className="bg-th-background rounded-md mr-2">
+            <MdImage className="w-10 h-10 sm:w-20 sm:h-20 text-th-background-tertiary" />
           </div>
         </div>
-        <div className="flex flex-row w-full">
+        <div className="flex flex-col w-full">
           <div>
-            <span>{product.proveedor}</span>
+            <span className={stylesGeneral.item_code}>
+              {`[ ${product.codigo} ]`}
+            </span>
           </div>
           <div>
-            <span>{product.genero}</span>
+            <span className={stylesGeneral.item_name}>
+              {`${product.categoria} ${product.nombre}`}
+            </span>
+          </div>
+          <div className="flex flex-row w-full">
+            <span className={stylesGeneral.item_sub}>
+              {`${product.proveedor} - ${product.genero}`}
+            </span>
           </div>
         </div>
       </div>
 
-      <div>
-        <button>Editar</button>
-        <button>Eliminar</button>
+      <div className={stylesGeneral.item_icon_container}>
+        <button>
+          <MdAppRegistration className={stylesGeneral.item_icon} />
+        </button>
+        <button>
+          <MdOutlineFindInPage className={stylesGeneral.item_icon} />
+        </button>
+        <button>
+          <MdModeEdit className={stylesGeneral.item_icon} />
+        </button>
+        <button>
+          <MdDeleteForever className={stylesGeneral.item_icon_del} />
+        </button>
       </div>
     </div>
   );
