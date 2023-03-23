75c00dd47fef5975fc45be7b6048ad2d73387f23
Pablo Mesquida <pablomesquida67@gmail.com>
2023-03-23 12:37:36 +0100

    cloudinary-upload

--- components/stock/add/FormProducts.js
+++ components/stock/add/FormProducts.js
@@ -7,38 +7,31 @@ import CheckSizes from "@/components/forms/CheckSizes.js";
 import CheckColors from "@/components/forms/CheckColors.js";
 import { useDispatch } from "react-redux";
 import { createProduct } from "@/actions/productsActions";
+import { useState } from "react";
 
 const FormProducts = () => {
+  const [imageSrc, setImageSrc] = useState();
+  const [uploadData, setUploadData] = useState();
   const router = useRouter();
   const dispatch = useDispatch();
   const formik = useFormik({
     initialValues: {
-      id_categories: "",
-      id_genders: "",
-      id_suppliers: "",
-      name: "",
-      description: "",
-      cost_price: "",
-      price: "",
-      clearance_price: "",
-      sizes: [],
-      colors: [],
-      photo: "",
-      code: "00000000",
+      //  id_categories: "",
+      // id_genders: "",
+      //   id_suppliers: "",
+      prod_name: "",
+      //  description: "",
+      //   cost_price: "",
+      //   price: "",
+      //   clearance_price: "",
+      //   sizes: [],
+      //   colors: [],
+      file: "",
+      //   code: "00000000",
     },
     validate: add_product_validate,
     onSubmit,
   });
-
-  // const handleFileChange = (event) => {
-  //   setFile(event.target.files[0]);
-
-  //   const fileReader = new FileReader();
-  //   fileReader.readAsDataURL(event.target.files[0]);
-  //   fileReader.onloadend = () => {
-  //     setPreview(fileReader.result);
-  //   };
-  // };
 
   async function onSubmit(values) {
     const options = {
@@ -48,12 +41,57 @@ const FormProducts = () => {
     };
     console.log(values);
 
+    if (response.ok) {
+      console.log("File and text uploaded successfully");
+    } else {
+      console.error("Error uploading file and text");
+    }
+
     await fetch("../../api/stock", options)
       .then((res) => res.json())
       .then((data) => {
         dispatch(createProduct(data));
         if (data) router.push("/stock");
       });
+  }
+
+  async function handleOnSubmit(event) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const fileInput = Array.from(form.elements).find(
+      ({ name }) => name === "file"
+    );
+
+    const formData = new FormData();
+
+    for (const file of fileInput.files) {
+      formData.append("file", file);
+    }
+
+    formData.append("upload_preset", "stckctrl-uploads");
+
+    const data = await fetch(
+      "https://api.cloudinary.com/v1_1/dq2hljnad/image/upload",
+      {
+        method: "POST",
+        body: formData,
+      }
+    ).then((r) => r.json());
+
+    setImageSrc(data.secure_url);
+    setUploadData(data);
+  }
+
+  function handleOnChange(changeEvent) {
+    const reader = new FileReader();
+
+    reader.onload = function (onLoadEvent) {
+      setImageSrc(onLoadEvent.target.result);
+      setUploadData(undefined);
+    };
+
+    reader.readAsDataURL(changeEvent.target.files[0]);
   }
 
   return (
@@ -63,7 +101,7 @@ const FormProducts = () => {
         onSubmit={formik.handleSubmit}
         encType="multipart/form-data"
       >
-        <div>
+        {/* <div>
           <SelectOptions formik={formik} name="categories" text="Categoría" />
         </div>
         <div>
@@ -71,7 +109,7 @@ const FormProducts = () => {
         </div>
         <div>
           <SelectOptions formik={formik} name="suppliers" text="Proveerdor" />
-        </div>
+        </div> */}
         <div
           className={`${stylesGeneral.input_group} ${
             formik.errors.name && formik.touched.name ? "border-red-600" : ""
@@ -80,12 +118,12 @@ const FormProducts = () => {
           <input
             className={stylesGeneral.input_text}
             type="text"
-            name="name"
+            name="prod_name"
             placeholder="Nombre"
-            {...formik.getFieldProps("name")}
+            {...formik.getFieldProps("prod_name")}
           />
         </div>
-        <div>
+        {/* <div>
           <textarea
             className={stylesGeneral.input_text}
             placeholder="Descripción"
@@ -94,8 +132,8 @@ const FormProducts = () => {
             cols="20"
             {...formik.getFieldProps("description")}
           ></textarea>
-        </div>
-        <div
+        </div> */}
+        {/* <div
           className={`${stylesGeneral.input_group} ${
             formik.errors.cost_price && formik.touched.cost_price
               ? "border-red-600"
@@ -109,8 +147,8 @@ const FormProducts = () => {
             placeholder="Precio de costo"
             {...formik.getFieldProps("cost_price")}
           />
-        </div>
-        <div
+        </div> */}
+        {/* <div
           className={`${stylesGeneral.input_group} ${
             formik.errors.price && formik.touched.price ? "border-red-600" : ""
           }`}
@@ -122,8 +160,8 @@ const FormProducts = () => {
             placeholder="Precio"
             {...formik.getFieldProps("price")}
           />
-        </div>
-        <div
+        </div> */}
+        {/* <div
           className={`${stylesGeneral.input_group} ${
             formik.errors.clearance_price && formik.touched.clearance_price
               ? "border-red-600"
@@ -137,29 +175,34 @@ const FormProducts = () => {
             placeholder="Precio de liquidación"
             {...formik.getFieldProps("clearance_price")}
           />
-        </div>
-        <div>
-          <CheckSizes formik={formik} />
-        </div>
-        <div>
-          <CheckColors formik={formik} />
-        </div>
-        <div>
-          <label> Upload File</label>
-          <input
-            type="file"
-            name="photo"
-            accept="image/*"
-            onChange={(e) =>
-              formik.setFieldValue("photo", e.currentTarget.files[0])
-            }
-          />
-        </div>
+        </div> */}
+        <div>{/* <CheckSizes formik={formik} /> */}</div>
+        <div>{/* <CheckColors formik={formik} /> */}</div>
+
         <div>
           <button type="submit" className={stylesGeneral.button}>
             Agregar
           </button>
         </div>
+      </form>
+      <form method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+        <p>
+          <input type="file" name="file" />
+        </p>
+
+        <img src={imageSrc} />
+
+        {imageSrc && !uploadData && (
+          <p>
+            <button>Upload Files</button>
+          </p>
+        )}
+
+        {uploadData && (
+          <code>
+            <pre>{JSON.stringify(uploadData, null, 2)}</pre>
+          </code>
+        )}
       </form>
     </div>
   );
