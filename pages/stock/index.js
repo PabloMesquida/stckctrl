df66497be0405d7212df1c0bf2d3153c2684eea5
Pablo Mesquida <pablomesquida67@gmail.com>
2023-04-07 19:05:33 +0200

    headers-methods

--- pages/stock/index.js
+++ pages/stock/index.js
@@ -36,6 +36,7 @@ export async function getServerSideProps({ req }) {
 
   const headers = {
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
     "Access-Control-Allow-Headers":
       "Origin, X-Requested-With, Content-Type, Accept",
   };
