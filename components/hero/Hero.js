295eb9b3aa608e75c6bdcc7f27f2c19637de8c95
Pablo Mesquida <pablomesquida67@gmail.com>
2023-05-29 13:09:51 +0200

    remove-threejs

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,27 +1,10 @@
-import * as THREE from "three";
-import { Canvas } from "@react-three/fiber";
-import Experience from "./Experience.js";
 import Login from "./Login.js";
 import stylesHero from "@/styles/Hero.module.css";
-
-THREE.ColorManagement.enabled = true;
 
 const Hero = () => {
   return (
     <div className={stylesHero.hero_container}>
-      <div className={stylesHero.hero_col1}>
-        <Canvas
-          className="w-full"
-          camera={{
-            fov: 45,
-            near: 0.1,
-            far: 2000,
-            position: [5, 3.2, -5],
-          }}
-        >
-          <Experience />
-        </Canvas>
-      </div>
+      <div className={stylesHero.hero_col1}></div>
       <div className={stylesHero.hero_col2}>
         <div>
           <Login />
